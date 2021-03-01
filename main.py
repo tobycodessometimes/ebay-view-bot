94fdbaae8627423524e774b4702f3ecae673931e
tobycodessometimes <73805149+tobycodessometimes@users.noreply.github.com>
2021-03-01 15:21:57 +0000

    Cleaned Code
    - Cleaning

--- main.py
+++ main.py
@@ -2,20 +2,16 @@ import requests
 import time
 
 def view():
-    print
     print (" --------------------------------------------")
     print (" viewbot v.1.0.0")
     print (" --------------------------------------------\n")
     listingURL = input(" Link : ")
-    print
-    # viewCount = int(raw_input(" How many views? : "))
+
     viewCount = int(input(" How many views? : "))
 
-    print
     print (" Watching ... ")
     print (" Do not close this window. ")
 
-    print
     start_time = time.time()
     for i in range(viewCount):
         r = requests.get(listingURL)
@@ -25,8 +21,7 @@ def view():
     print(" Total time : " + " %s sec" % viewTime)
     viewRate = float(viewCount / viewTime)
     print (" View rate  : " +  " %s views/sec" % viewRate)
-    print
-    print
+
     
     # todo : iterate through other links in txt file, multithread, proxy support
 
